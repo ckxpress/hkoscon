5a5cf6bc4cdc069eb38282dc175108fc29a1ffef
Rick Mak <rick.mak@gmail.com>
2016-06-21 18:32:01 +0800

    Try to load the chat demo

--- __init__.py
+++ __init__.py
@@ -1,1 +1,9 @@
+from skygear import static_assets
+from skygear.utils.assets import relative_assets
+
 from .chat import plugin as chat_plugin
+
+
+@static_assets(prefix='demo')
+def chat_demo():
+    return relative_assets('chat/js-sdk/demo')
